496ecce0f2864eff86d6f6d26e176cb06225dc5a
camilo penagos <penagoscamilo99@gmail.com>
2021-12-28 00:31:31 -0500

    feat: trabajando con state y alerts

--- pages/registro.jsx
+++ pages/registro.jsx
@@ -2,6 +2,7 @@ import Layout from "../components/Layout";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import { useMutation, gql } from "@apollo/client";
+import { useState } from "react";
 
 const NUEVA_CUENTA = gql`
   mutation NuevoVendedor($input: UsuarioInput!) {
@@ -14,6 +15,9 @@ const NUEVA_CUENTA = gql`
   }
 `;
 const Registro = () => {
+  //state operacion
+
+  const [mensaje, setMensaje] = useState(null);
   //Crear usuario, Mutation
   const [usuario] = useMutation(NUEVA_CUENTA);
   //validación del formulario
@@ -39,20 +43,50 @@ const Registro = () => {
     onSubmit: async (valores) => {
       console.log("Enviando...", valores);
       try {
-        const user = await usuario({
+        const { data } = await usuario({
           variables: {
             input: {
               ...valores,
             },
           },
         });
-        console.log("User", user);
-        if (user) alert(`¡Usuario ${valores.nombre} creado correctamente!`);
+        console.log("User", data);
+        if (data) {
+          setMensaje(true);
+        }
+        //alert(`¡Usuario ${data.usuario.nombre} creado correctamente!`);
       } catch (error) {
-        console.log("error");
+        console.log("error", error);
       }
     },
   });
+
+  const mostrar = () => {
+    return (
+      <div
+        className="bg-green-100 border-t-4 border-green-500 rounded-b text-green-900 px-4 py-3 shadow-md"
+        role="alert"
+      >
+        <div className="flex">
+          <div className="py-1">
+            <svg
+              className="fill-current h-6 w-6 text-green-500 mr-4"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
+            </svg>
+          </div>
+          <div>
+            <p className="font-bold">Usuario creado correctamente</p>
+            <p className="text-sm text-green-500">
+              El usuario vendedor, ha sido registrado en el sistema.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  };
 
   //Validar el formulario con Formik y Yup
   return (
@@ -181,6 +215,12 @@ const Registro = () => {
                 className="bg-gray-800 w-full mt-5 p-2 text-white uppercase tw-hover:underline tw-no-underline"
                 value="Registro Usuario"
               />
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2 py-1"
+                htmlFor="nombre"
+              >
+                {mensaje && mostrar()}
+              </label>
             </form>
           </div>
         </div>
