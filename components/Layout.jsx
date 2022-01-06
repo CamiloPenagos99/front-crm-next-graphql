43fabc9973551854731df83bac0110a15863ef38
camilo penagos <penagoscamilo99@gmail.com>
2022-01-06 17:24:49 -0500

    Feat: context link apollo, config

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,6 +8,10 @@ const Layout = ({ children }) => {
   console.log("Estado de usuario:", authenticated);
   //router hook
   const router = useRouter();
+
+  const handlerClick = () => {
+    router.push("/login");
+  };
   return (
     <>
       <Head>
@@ -30,11 +34,17 @@ const Layout = ({ children }) => {
         <div className="bg-gray-200 min-h-screen flex flex-col justify-center">
           <div role="alert" className="sm:w-2/3 xl:w-4/5 sm:min-h-screen p-5">
             <div className="bg-red-500 text-white font-bold rounded-t px-4 py-2">
-              No Autorizado
+              No autorizado
             </div>
             <div className="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
               <p>El usuario no ha iniciado sesion</p>
             </div>
+            <button
+              onClick={handlerClick}
+              className="mr-4 bg-gray-500 hover:bg-gray-400 text-white font-bold py-2 px-4 border-b-4 border-gray-700 hover:border-blue-500 rounded"
+            >
+              Iniciar sesion
+            </button>
           </div>
         </div>
       ) : (
--- config/apollo.js
+++ config/apollo.js
@@ -1,13 +1,31 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 import fetch from "node-fetch";
+import { setContext } from "apollo-link-context";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000/graphql",
+  fetch,
+});
+
+/**
+ * Configurar los headers, para enviar la autenticacion y jwt
+ */
+const authLink = setContext((_, { headers }) => {
+  //leer token
+
+  const token = localStorage.getItem("tkn");
+  return {
+    headers: {
+      ...headers,
+      authorization: token,
+    },
+  };
+});
 
 const cliente = new ApolloClient({
   connectToDevTools: true,
   cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: "http://localhost:4000/graphql",
-    fetch,
-  }),
+  link: authLink.concat(httpLink),
 });
 
 export default cliente;
