78258ffc641a6c51e98a16df3e3825cd9c25d27c
camilo penagos <penagoscamilo99@gmail.com>
2022-01-06 18:07:30 -0500

    feat: consultar cliente, query gql

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,29 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Layout from '../components/Layout'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Layout from "../components/Layout";
+import { gql, useQuery } from "@apollo/client";
+
+const CLIENTES = gql`
+  query obtenerClientesVendedor {
+    obtenerClientesVendedor {
+      apellido
+      nombre
+      id
+      empresa
+      vendedor
+    }
+  }
+`;
 
 export default function Home() {
+  const { loading, error, data } = useQuery(CLIENTES);
   return (
     <div>
       <Layout>
         <h1 className="text-2xl text-gray-800 font-light">Clientes</h1>
+        <div>{JSON.stringify(data)}</div>
       </Layout>
     </div>
-  )
+  );
 }
