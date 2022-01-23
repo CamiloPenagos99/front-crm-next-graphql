5e495272c1d4c2e9be90ad39e38babafa3df54b6
camilo penagos <penagoscamilo99@gmail.com>
2022-01-23 11:01:17 -0500

    feat: maquetando editar cliente

--- components/Cliente.jsx
+++ components/Cliente.jsx
@@ -79,6 +79,23 @@ export const Cliente = ({ cliente }) => {
     });
   };
 
+  const confirmarEditarCliente = (id) => {
+    Swal.fire({
+      title: "¿Quieres editar el Cliente?",
+      text: "Asegurate de registrar la información correcta",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#475B63",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, editar cliente",
+      cancelButtonText: "No, cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        console.log("Entrar en modo edición");
+      }
+    });
+  };
+
   return (
     <>
       <tr key={cliente.id}>
@@ -106,6 +123,27 @@ export const Cliente = ({ cliente }) => {
                 strokeLinejoin="round"
                 strokeWidth={2}
                 d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </button>
+          <button
+            type="button"
+            className="flex justify-center bg-gray-800 py-2 px-4 w-full text-white rounded text-xs uppercase font-bold"
+            onClick={() => confirmarEditarCliente(cliente.id)}
+          >
+            Editar
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-4 h-4 ml-2"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
               />
             </svg>
           </button>
