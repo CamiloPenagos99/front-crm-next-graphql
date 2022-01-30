f2d366cd60984806ba81394ea8578608c1008ebf
camilo penagos <penagoscamilo99@gmail.com>
2022-01-30 11:02:10 -0500

    feat: eliminar producto mutation

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,9 +1,58 @@
 import BotonEliminar from "../utils/botonEliminar";
+import { gql, useQuery, useMutation } from "@apollo/client";
+import Swal from "sweetalert2";
 
-function confirmarEliminarProducto(id) {
-  alert("Eliminando el producto... " + id);
-}
+const ELIMINARPRODUCTO = gql`
+  mutation EliminarProducto($eliminarProductoId: ID!) {
+    eliminarProducto(id: $eliminarProductoId) {
+      id
+      nombre
+      stock
+    }
+  }
+`;
+
 export const Product = ({ producto }) => {
+  function confirmarEliminarProducto(id) {
+    Swal.fire({
+      title: "¿Estas seguro, de eliminar el producto?",
+      text: "Eliminar el producto, es irreversible",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#053476",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, borrar",
+      cancelButtonText: "No, cancelar",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          console.log(`Eliminando... ${id}`);
+
+          const response = await eliminarProducto({
+            variables: {
+              eliminarProductoId: id,
+            },
+          });
+          console.log("Se elimino: ", response);
+          Swal.fire(
+            "Eliminado",
+            "El producto se ha eliminado correctamente",
+            "completo"
+          );
+        } catch (error) {
+          console.error(error);
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: error.message,
+            footer: '<a href="">Why do I have this issue?</a>',
+          });
+        }
+      }
+    });
+  }
+
+  const [eliminarProducto] = useMutation(ELIMINARPRODUCTO);
   return (
     <>
       <tr key={producto.id}>
