4e495da5fb46c6c1f4792b6f6cbdfd03dddbf357
camilo penagos <penagoscamilo99@gmail.com>
2022-02-10 23:24:50 -0500

    feat: query consultar pedidos

--- pages/pedidos.jsx
+++ pages/pedidos.jsx
@@ -1,10 +1,33 @@
+import { gql, useQuery } from "@apollo/client";
 import Layout from "../components/Layout";
+import Loader from "../utils/loader";
 
+const PEDIDOS = gql`
+  query ObtenerPedidos {
+    obtenerPedidos {
+      id
+      pedido {
+        id
+        cantidad
+      }
+      total
+      cliente
+      vendedor
+      estado
+    }
+  }
+`;
 const Pedidos = () => {
+  const { loading, error, data } = useQuery(PEDIDOS);
   return (
     <div>
       <Layout>
-      <h1 className="text-2xl text-gray-800 font-light">Pedidos</h1>
+        <h1 className="text-4xl text-cyan-800 font-light">Productos</h1>
+        {data ? (
+          <div>{JSON.stringify(data.obtenerPedidos)}</div>
+        ) : (
+          <Loader message="Cargando información"></Loader>
+        )}
       </Layout>
     </div>
   );
