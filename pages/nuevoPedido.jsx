2f022ea2b51876643e8d3e2e86122590db5bf72d
camilo penagos <penagoscamilo99@gmail.com>
2022-02-10 23:36:38 -0500

    feat: configurando componente de nuevo pedido

---
+++ pages/nuevoPedido.jsx
@@ -0,0 +1,14 @@
+import Layout from "../components/Layout";
+const NuevoPedido = () => {
+  return (
+    <>
+      <Layout>
+        <div>
+          <h2>Registrar nuevo pedido:</h2>
+        </div>
+      </Layout>
+    </>
+  );
+};
+
+export default NuevoPedido;
--- pages/pedidos.jsx
+++ pages/pedidos.jsx
@@ -1,6 +1,7 @@
 import { gql, useQuery } from "@apollo/client";
 import Layout from "../components/Layout";
 import Loader from "../utils/loader";
+import Link from "next/dist/client/link";
 
 const PEDIDOS = gql`
   query ObtenerPedidos {
@@ -22,7 +23,13 @@ const Pedidos = () => {
   return (
     <div>
       <Layout>
-        <h1 className="text-4xl text-cyan-800 font-light">Productos</h1>
+        <h1 className="text-4xl text-cyan-800 font-light">Pedidos</h1>
+        <br />
+        <Link href="/nuevoPedido">
+          <a className="text-white bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:ring-gray-400 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-800 dark:border-gray-700 text-sm hover:bg-blue-500 mb-3">
+            Nuevo pedido
+          </a>
+        </Link>
         {data ? (
           <div>{JSON.stringify(data.obtenerPedidos)}</div>
         ) : (
